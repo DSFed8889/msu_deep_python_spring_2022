1fad281cb5d44334ed58503ffa7a0f074f4a1c37
DSFed8889 <FedorovDS@my.msu.ru>
2022-05-04 14:05:52 +0300

    First attempt

---
+++ 05/LRU_cache.py
@@ -0,0 +1,29 @@
+class LRUCache:
+
+    def __init__(self, limit=42):
+        if limit < 0:
+            raise ValueError('Limit must be non negative!')
+        self.limit = limit
+        self._data = {}
+        self.__LRU = []
+
+    def get(self, key):
+        if key in self._data:
+            self.__LRU.pop(self.__LRU.index(key))
+            self.__LRU.append(key)
+            return self._data[key]
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+    def set(self, key, value):
+        if key in self._data:
+            self.__LRU.pop(self.__LRU.index(key))
+        if len(self.__LRU) == self.limit and self.limit != 0:
+            self._data.pop(self.__LRU[0])
+            self.__LRU = self.__LRU[1:]
+        self.__LRU.append(key)
+        self._data[key] = value
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
---
+++ 05/test_LRU_cache.py
@@ -0,0 +1,86 @@
+import unittest
+from LRU_cache import LRUCache
+
+
+class TestLRUCache(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_no_limt(self):
+        cache = LRUCache(limit=0)
+        for i in range(10000):
+            self.assertEqual(cache.get(f'key1_{i}'), None)
+        for i in range(10000):
+            cache.set(f'key1_{i}', i)
+        for i in range(10000):
+            self.assertEqual(cache.get(f'key1_{i}'), i)
+        for i in range(10000):
+            cache.set(f'key1_{i}', 200000)
+        for i in range(10000):
+            self.assertEqual(cache.get(f'key1_{i}'), 200000)
+        for i in range(10000):
+            self.assertEqual(cache[f'key2_{i}'], None)
+        for i in range(10000):
+            cache[f'key2_{i}'] = i
+        for i in range(10000):
+            self.assertEqual(cache[f'key2_{i}'], i)
+        for i in range(10000):
+            cache[f'key2_{i}'] = 200000
+        for i in range(10000):
+            self.assertEqual(cache[f'key2_{i}'], 200000)
+
+    def test_LRU(self):
+        cache = LRUCache(5)
+        for i in range(5):
+            self.assertEqual(cache[f'k{i}'], None)
+            cache[f'k{i}'] = [i] * (i + 1)
+        for i in range(5):
+            self.assertEqual(cache[f'k{i}'], [i] * (i + 1))
+        cache['k5'] = 'five'
+        self.assertEqual(cache['k0'], None)
+        for i in range(1, 5):
+            self.assertEqual(cache[f'k{i}'], [i] * (i + 1))
+        self.assertEqual(cache['k5'], 'five')
+
+        cache = LRUCache(5)
+        for i in range(5):
+            self.assertEqual(cache[f'k{i}'], None)
+            cache[f'k{i}'] = [i] * (i + 1)
+        for i in range(5):
+            self.assertEqual(cache[f'k{5 - i - 1}'], [5 - i - 1] * (5 - i))
+        cache['k5'] = 'five'
+        self.assertEqual(cache['k4'], None)
+        for i in range(4):
+            self.assertEqual(cache[f'k{i}'], [i] * (i + 1))
+        self.assertEqual(cache['k5'], 'five')
+
+        cache = LRUCache(5)
+        for i in range(5):
+            self.assertEqual(cache[f'k{i}'], None)
+            cache[f'k{i}'] = [i] * (i + 1)
+        for i in range(5):
+            self.assertEqual(cache[f'k{i}'], [i] * (i + 1))
+        self.assertEqual(cache['k0'], [0])
+        cache['k5'] = 'five'
+        self.assertEqual(cache['k0'], [0])
+        self.assertEqual(cache['k1'], None)
+        for i in range(2, 5):
+            self.assertEqual(cache[f'k{i}'], [i] * (i + 1))
+        self.assertEqual(cache['k5'], 'five')
+
+    def test_errors(self):
+        with self.assertRaises(ValueError):
+            LRUCache(-100)
+        cache = LRUCache(0)
+        a = ['my_list']
+        with self.assertRaises(TypeError):
+            cache[a]
+        with self.assertRaises(TypeError):
+            cache.get(a)
+        with self.assertRaises(TypeError):
+            cache[a] = 0
+        with self.assertRaises(TypeError):
+            cache.set(a, 0)
